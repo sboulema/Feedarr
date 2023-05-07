84f7a50214a3948e3a92a816eb4548cb2a8216fe
Samir L. Boulema <sboulema@gmail.com>
2023-05-07 20:25:38 +0200

    Improve magnet support

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 using System.Text.RegularExpressions;
 using X.Web.RSS;
+using X.Web.RSS.Structure.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/{folder}.rss", (string folder) =>
+app.MapGet("/{folder}.rss", async (string folder) =>
 {
     if (!Path.Exists($"/torrents/{folder}"))
     {
@@ -31,12 +32,12 @@ app.MapGet("/{folder}.rss", (string folder) =>
         {
             Title = Cleanup(file.Name),
             Description = Cleanup(file.Name),
-            Link = new($"{baseUrl}/{folder}/{file.Name}"),
+            Link = await GetRssUrl(baseUrl, folder, file.Name),
             PubDate = file.CreationTimeUtc,
             Enclosure = new()
             {
-                Url = new($"{baseUrl}/{folder}/{file.Name}"),
-                Type = "application/x-bittorrent"
+                Url = await GetRssUrl(baseUrl, folder, file.Name),
+                Type = GetType(file.Extension)
             }
         });
     }
@@ -44,14 +45,14 @@ app.MapGet("/{folder}.rss", (string folder) =>
     return Results.Text(feed.ToXml(), "text/xml");
 });
 
-app.MapGet("/{folder}/{file}.torrent", async (string folder, string file) =>
+app.MapGet("/{folder}/{fileName}.torrent", async (string folder, string fileName) =>
 {
-    if (!Path.Exists($"/torrents/{folder}/{file}.torrent"))
+    if (!Path.Exists($"/torrents/{folder}/{fileName}.torrent"))
     {
         return Results.NotFound();
     }
 
-    return Results.File(await File.ReadAllBytesAsync($"/torrents/{folder}/{file}.torrent"));
+    return Results.File(await File.ReadAllBytesAsync($"/torrents/{folder}/{fileName}.torrent"));
 });
 
 app.Run();
@@ -60,8 +61,35 @@ string Cleanup(string fileName)
 {
     fileName = Path.GetFileNameWithoutExtension(fileName);
     fileName = fileName.Replace(".", " ");
+    // Remove emoticons from file name
     fileName = Regex.Replace(fileName, @"\p{Cs}|\p{So}", string.Empty);
     fileName = fileName.Trim();
 
     return fileName;
 }
+
+async Task<RssUrl> GetRssUrl(string baseUrl, string folder, string fileName)
+{
+    var fileExtension = Path.GetExtension(fileName);
+
+    if (fileExtension == ".torrent")
+    {
+        return new($"{baseUrl}/{folder}/{fileName}");
+    }
+
+    if (fileExtension == ".magnet")
+    {
+        var magnetUrl = await File.ReadAllTextAsync($"/torrents/{folder}/{fileName}");
+        return new(magnetUrl);
+    }
+
+    return new();
+}
+
+string GetType(string fileExtension)
+    => fileExtension switch
+    {
+        ".torrent" => "application/x-bittorrent",
+        ".magnet" => "x-scheme-handler/magnet",
+        _ => string.Empty
+    };
